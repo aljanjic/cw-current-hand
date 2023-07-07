a394b6e117d3d45e0d5dd6e246459085954b8628
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-07 18:15:08 +0200

    Submited solution + other people oneliner

--- src/currentHand.js
+++ src/currentHand.js
@@ -1,7 +1,63 @@
-function currentHand() {
-  return [];
+function currentHand(arr) {
 
+
+
+  console.log( [...new Set(arr.slice(0, arr.length - 4))].filter(x => !arr.slice(-4).includes(x)) )
+
+  return [...new Set(arr.slice(0, arr.length - 4))].filter(x => !arr.slice(-4).includes(x)).sort()
+
+  // if (!arr.length || arr.length <= 4) return [];
+
+  // opponentsHand = [];
+
+  // last4cards = arr.slice(arr.length-4);
+
+  // for(let i = 0; i < arr.length - 4 ; i ++){
+
+  //   if(!last4cards.includes(arr[i]) && !opponentsHand.includes(arr[i])){
+  //     opponentsHand.push(arr[i])
+  //   }
+  // }
   
+  // return opponentsHand.sort();
+
+
+  // if (!arr.length || arr.length <= 4) return [];
+
+  // opponentsHand = [];
+
+  // last4cards = arr.slice(arr.length-4);
+
+  // for(let i = 0; i < arr.length - 4 ; i ++){
+
+  //   if(!last4cards.includes(arr[i]) && !opponentsHand.includes(arr[i])){
+  //     opponentsHand.push(arr[i])
+  //       if (opponentsHand.length >= 4) {
+  //         return opponentsHand.sort();
+  //       }
+  //   }
+  // }
+  // return opponentsHand.sort();
+
+  // if (!arr.length || arr.length <= 4) return [];
+
+  // opponentsHand = [];
+
+  // last4cards = arr.slice(arr.length-4);
+
+  // for(let i = 0; i < arr.length - 4 ; i ++){
+
+  //   if(!last4cards.includes(arr[i])){
+  //     if (!opponentsHand.includes(arr[i])){
+  //       opponentsHand.push(arr[i])
+  //       if (opponentsHand.length >= 4) {
+  //         return opponentsHand.sort();
+  //       }
+  //     }
+  //   }
+  // }
+  // return opponentsHand.sort();
+
 }
 
 module.exports = currentHand;
--- src/currentHand.test.js
+++ src/currentHand.test.js
@@ -4,4 +4,47 @@ describe('currentHand', () => {
   it("[] should return []", () => {
     expect(currentHand([])).toStrictEqual([]);
   });
+
+  it("['Hog Rider','Earthquake','Archer Queen','Giant Skeleton','Fire Spirit','Cannon'] \
+     should return ['Hog Rider', 'Earthquake']", () => {
+    expect(
+    currentHand(
+      [
+        'Hog Rider',
+        'Earthquake',
+        'Archer Queen',
+        'Giant Skeleton',
+        'Fire Spirit',
+        'Cannon']))
+    .toStrictEqual(['Hog Rider', 'Earthquake'].sort());
+  });
+
+
+  it("['Longes option'] \
+  should return ['Hog Rider', 'Earthquake']", () => {
+ expect(
+ currentHand(
+   [
+    'Hog Rider',
+    'Earthquake',
+    'Archer Queen',
+    'Giant Skeleton',
+    'Fire Spirit',
+    'Cannon',
+    'Skeletons',
+    'Hog Rider',
+    'The Log',
+    'Fire Spirit',
+    'Earthquake',
+    'Giant Skeleton',
+    'Skeletons',
+    'Archer Queen',
+    'Fire Spirit',
+    'Cannon',
+    'Giant Skeleton',
+    'Skeletons']))
+ .toStrictEqual(['Hog Rider', 'The Log', 'Earthquake', 'Archer Queen'].sort());
+});
+
+
 });
